08c35eb0969630ace61cf22c7b904072c460fb1b
rapPayne <bob@creator.net>
2024-09-29 15:26:55 -0500

    Added useCallback hook. Also CSS changes

--- react-client/src/PeoplePicker.jsx
+++ react-client/src/PeoplePicker.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState, } from "react"
+import { useCallback, useEffect, useState, } from "react"
 import { Person } from "./Person"
 import './PeoplePicker.css'
 
 export function PeoplePicker() {
   const [chosenPerson, setChosenPerson] = useState();
-  const [unchosenPeople, setUnchosenPeople] = useState([])
-  const [chosenPeople, setChosenPeople] = useState([])
+  const [unchosenPeople, setUnchosenPeople] = useState([]);
+  const [chosenPeople, setChosenPeople] = useState([]);
+
   useEffect(() => {
     fetchPeople();
   }, []);
 
-  function fetchPeople() {
+  // useCallback allows the component to remember fetchPeople between renders
+  // thereby obviating the need to re-create fetchPeople on every rerender.
+  const fetchPeople = useCallback(() => {
     fetch('http://localhost:3001/api/people')
       .then(res => res.json())
       .then(ppl => {
@@ -18,29 +21,33 @@ export function PeoplePicker() {
         setChosenPeople([]);
         setChosenPerson(null);
       });
-  }
+  }, []);
+
   return (
     <div className="PeoplePicker">
       <div id="people-picker-title">
         <h1>People Picker</h1>
       </div>
 
-
-      <h2>Chosen Person</h2>
-      {chosenPerson ? <></> : <p className="alert info">Hit the choose button to select a random person</p>}
       <div className="buttonRow">
         <button onClick={() => chooseRandomPerson()}>Choose</button>
-        <button className="link" onClick={() => resetPeople()}>Reset</button>
+        <button className="link" onClick={() => fetchPeople()}>Reset</button>
       </div>
+
       <section className="chosenPerson">
-        {chosenPerson ? <Person person={chosenPerson} /> : <></>}
+        {chosenPerson ? <><h2>Chosen Person</h2><Person person={chosenPerson} /></> : <p className="alert info">Hit the choose button to select a random person</p>}
       </section>
+
+      <hr />
+
       <h2>Unchosen People</h2>
       <section className="unchosenPeople">
         {unchosenPeople
           .map((p, index) => <Person person={p} key={index} />)}
       </section>
+
       <hr />
+
       <h2>Already chosen People</h2>
       <section className="chosenPeople">
         {chosenPeople.map(p => <Person person={p} key={p.id} />)}
@@ -50,16 +57,13 @@ export function PeoplePicker() {
 
   function chooseRandomPerson() {
     if (unchosenPeople.length === 0) {
-      resetPeople();
+      fetchPeople();
       return;
     }
     let person = unchosenPeople[Math.floor(Math.random() * unchosenPeople.length)];
     setChosenPerson(person);
     setUnchosenPeople(unchosenPeople.filter(p => p !== person));
     setChosenPeople([...chosenPeople, person])
-    console.log("chosen person is", person)
   }
-  function resetPeople() {
-    fetchPeople();
-  }
+
 }
